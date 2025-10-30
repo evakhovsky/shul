111c9def9397a70fe7796f661726ccf22b0b56c5
e vakhovsky <einefeder1@gmail.com>
2025-10-30 09:26:10 -0400

    lazha

--- src/components/pages/content/UserDonations.js
+++ src/components/pages/content/UserDonations.js
@@ -2,23 +2,20 @@ import React, { useState } from 'react';
 import authenticationService from '../../shared/services/authentication.service'
 import { Navigate } from "react-router-dom";
 //import 'react-day-picker/lib/style.css';
-//import DayPickerInput from 'react-day-picker/DayPickerInput'
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import "react-day-picker/dist/style.css";
 import '../Home.css';
-import moment from 'moment'
 import dateFnsFormat from 'date-fns/format';
 import Button from 'react-bootstrap/Button';
 import donationService from '../../shared/services/donationService'
 import Table from 'react-bootstrap/Table'
 import {TableRow, TableBody, TableHead, TableCell} from '@mui/material';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import {Link } from "react-router-dom";
-import helperUtil from '../../shared/Util'
 
 function UserDonations() {
     const [firstName, setFirstName] = useState('');
@@ -94,73 +91,58 @@ function UserDonations() {
     }
 
     const renderStartDate = () => {
-        const FORMAT = 'MM/dd/yyyy';
         if(!startDate){
             return;
         }
 
         return (
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoContainer components={['DatePicker', 'DatePicker']}>
-                    <DatePicker 
-                        label="Uncontrolled picker" 
-                        defaultValue={dayjs('2022-04-17')} 
-                    />
-                    <DatePicker
-                    label="Start Date"
-                    value={dayjs(startDate)}
-                    onChange={(newValue) => setStartDate(newValue.toDate())}
-                    />
-                </DemoContainer>
-            </LocalizationProvider>
-        );
-
-        /*return <DayPickerInput
-        formatDate={helperUtil.formatDate}
-        parseDate={helperUtil.parseDate}
-        format={FORMAT}
-        placeholder={dateFnsFormat(startDate, FORMAT)}
-        dayPickerProps={{
-            month: startDate,
-            showWeekNumbers: true,
-            todayButton: 'Today',
-        }} 
-        onDayChange={
-            day => {
-                if(moment.isDate(day)){
-                    setStartDate(day)
-                }          
-            }
-        } />;*/
+            <View style={{marginRight: '10px', borderWidth: 0, borderColor: 'blue'}}>
+                <LocalizationProvider dateAdapter={AdapterDayjs} >
+                    <DemoContainer components={['DatePicker', 'DatePicker']}>
+                        <DatePicker
+                        label="Start Date"
+                        value={dayjs(startDate)}
+                        onChange={(newValue) => setStartDate(newValue.toDate())}
+                        />
+                    </DemoContainer>
+                </LocalizationProvider>
+            </View>
+        );        
     }
 
     const renderEndtDate = () => {
-        const FORMAT = 'MM/dd/yyyy';
-        /*return <DayPickerInput
-        formatDate={helperUtil.formatDate}
-        parseDate={helperUtil.parseDate}
-        format={FORMAT}
-        placeholder={dateFnsFormat(endDate, FORMAT)}
-        dayPickerProps={{
-            month: endDate,
-            showWeekNumbers: true,
-            todayButton: 'Today',
-        }} 
-        onDayChange={
-            day => {
-                if(moment.isDate(day)){
-                    setEndDate(day)
-                }
-                console.log(endDate)
-            }
-        } />;*/
+        return (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DatePicker', 'DatePicker']}>
+                    <DatePicker
+                    label="End Date"
+                    value={dayjs(endDate)}
+                    onChange={(newValue) => setEndDate(newValue.toDate())}
+                    />
+                </DemoContainer>
+            </LocalizationProvider>
+        );  
+    }
+
+    const renderDateRange = () => {
+        return(
+            <View style={{flex: 1, flexDirection: 'row', 
+                          justifyContent: "center", 
+                          alignItems: 'center', 
+                          marginTop: '10px', 
+                          marginBottom: '20px'}}>
+                {renderStartDate()}
+                {renderEndtDate()}
+            </View>
+        );
     }
 
     const renderDonation = (donation, index) => {
         return (
             <TableRow key={index}>
-              <TableCell align="left" style={{ textAlign: 'left', width: "25%" }}>${donation.amount}</TableCell>
-              <TableCell align="left" style={{ textAlign: 'left', width: "55%" }}>{formatReturnedDate(donation.date)}</TableCell>
+              <TableCell align="left" style={{ textAlign: 'left', width: "20%" }}>${donation.amount}</TableCell>
+              <TableCell align="left" style={{ textAlign: 'left', width: "20%" }}>{formatReturnedDate(donation.date)}</TableCell>
+              <TableCell align="left" style={{ textAlign: 'left', width: "40%" }}>{donation.purpose}</TableCell>
               <TableCell align="left" style={{ textAlign: 'left', width: "20%" }}></TableCell>
             </TableRow>
           );
@@ -201,8 +183,9 @@ function UserDonations() {
                 <Table striped hover size="sm">
                     <TableHead>
                         <TableRow>
-                            <TableCell align="left" style={{ width: "25%" }}>Amout</TableCell>
-                            <TableCell align="left" style={{ width: "55%" }}>Date</TableCell>
+                            <TableCell align="left" style={{ width: "20%" }}>Amout</TableCell>
+                            <TableCell align="left" style={{ width: "20%" }}>Date</TableCell>
+                            <TableCell align="left" style={{ width: "40%" }}>Purpose</TableCell>
                             <TableCell align="left" style={{ width: "20%" }}>Total: ${total}</TableCell>
                         </TableRow>
                     </TableHead>
@@ -215,25 +198,12 @@ function UserDonations() {
     }
 
     return (
-            <div>
-                <div className="center">
-                    {renderHeader()}    
-                </div>                
-                <div className="center">
-                    <label>Start Date</label>
-                    {renderStartDate()}
-                </div>
-                <div className="center">
-                    <label>End Date</label>
-                    {renderEndtDate()}
-                </div>
-                <div className="center">
-                    {renderFilterButton()}                    
-                </div>
-                <div className="flex-container85 center">
-                    {renderTable()}    
-                </div>                
-            </div>
+            <View style={{flex: 1, justifyContent: "center", alignItems: 'center'}}>                
+                {renderHeader()}
+                {renderDateRange()}                
+                {renderFilterButton()}                
+                {renderTable()}                                    
+            </View>
         );
     }
 
